a0b381decfa215cf48ed33c7e8779ac4ad5c4248
max <max>
2025-11-06 17:29:49 +0700

    feat: refactor Gallery and Slide components to accept dynamic image arrays, enhancing flexibility and reusability. Update Slide component to utilize passed images for rendering.

--- src/modules/HomePage/Gallery/Slide/index.tsx
+++ src/modules/HomePage/Gallery/Slide/index.tsx
@@ -4,22 +4,14 @@ import ImagePlaceHolder from "@Components/ImagePlaceHolder";
 import AutoScroll from "embla-carousel-auto-scroll";
 import useEmblaCarousel from "embla-carousel-react";
 import type React from "react";
-import { Activity } from "react";
 import styles from "./slide.module.scss";
-
-const IMGS = [
-	"https://images.unsplash.com/photo-1638962502979-05d81dcaa096?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2777",
-	"https://images.unsplash.com/photo-1604578762246-41134e37f9cc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1335",
-	"https://images.unsplash.com/photo-1632935187086-49a9d8027292?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1322",
-	"https://images.unsplash.com/photo-1680503397671-caa25818d36f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2370",
-	"https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1287",
-];
 
 type Props = {
 	direction?: "forward" | "backward";
+	imgs: string[];
 };
 
-function Slide({ direction = "forward" }: Props): React.ReactElement {
+function Slide({ direction = "forward", imgs }: Props): React.ReactElement {
 	const [emblaRef, _emblaApi] = useEmblaCarousel(
 		{
 			loop: true,
@@ -33,22 +25,20 @@ function Slide({ direction = "forward" }: Props): React.ReactElement {
 	);
 
 	return (
-		<Activity mode={IMGS.length > 1 ? "visible" : "hidden"}>
-			<div className={styles.embla} ref={emblaRef}>
-				<div className={styles.embla_container}>
-					{(direction === "forward" ? IMGS : IMGS.reverse()).map((img) => (
-						<div className={styles.embla_slide} key={img}>
-							<ImagePlaceHolder
-								src={img}
-								alt="Gallery"
-								width={1000}
-								height={1000}
-							/>
-						</div>
-					))}
-				</div>
+		<div className={styles.embla} ref={emblaRef}>
+			<div className={styles.embla_container}>
+				{imgs.map((img) => (
+					<div className={styles.embla_slide} key={img}>
+						<ImagePlaceHolder
+							src={img}
+							alt="Gallery"
+							width={1000}
+							height={1000}
+						/>
+					</div>
+				))}
 			</div>
-		</Activity>
+		</div>
 	);
 }
 
--- src/modules/HomePage/Gallery/index.tsx
+++ src/modules/HomePage/Gallery/index.tsx
@@ -14,6 +14,14 @@ import type React from "react";
 import styles from "./gallery.module.scss";
 import Slide from "./Slide";
 
+const IMGS = [
+	"https://images.unsplash.com/photo-1638962502979-05d81dcaa096?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2777",
+	"https://images.unsplash.com/photo-1604578762246-41134e37f9cc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1335",
+	"https://images.unsplash.com/photo-1632935187086-49a9d8027292?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1322",
+	"https://images.unsplash.com/photo-1680503397671-caa25818d36f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2370",
+	"https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1287",
+];
+
 function Gallery(): React.ReactElement {
 	return (
 		<div className={styles.gallery}>
@@ -46,8 +54,8 @@ function Gallery(): React.ReactElement {
 					</div>
 				</div>
 				<div className={styles.gallery_right}>
-					<Slide />
-					<Slide direction="backward" />
+					<Slide imgs={IMGS} />
+					<Slide direction="backward" imgs={IMGS.reverse()} />
 				</div>
 			</GridContainer>
 		</div>
