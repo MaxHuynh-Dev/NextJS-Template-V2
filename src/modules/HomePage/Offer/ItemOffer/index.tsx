a34304725c4769644db4fb385df05b86d7026fd9
max <max>
2025-11-06 20:44:15 +0700

    refactor: standardize padding across Footer, About, Gallery, and Offer components by replacing fixed values with CSS variables. Enhance ItemOffer component layout and styling for improved presentation.

--- src/modules/HomePage/Offer/ItemOffer/index.tsx
+++ src/modules/HomePage/Offer/ItemOffer/index.tsx
@@ -1,4 +1,4 @@
-// import ImagePlaceHolder from "@Components/ImagePlaceHolder";
+import ImagePlaceHolder from "@Components/ImagePlaceHolder";
 
 import Label from "@Components/Typo/Label";
 import {
@@ -40,14 +40,14 @@ function ItemOffer({ title, image, number }: TItemOffer): React.ReactElement {
 					{number}
 				</Label>
 			</div>
-			{/* <div className={styles.itemOffer_image}>
+			<div className={styles.itemOffer_image}>
 				<ImagePlaceHolder
-					src="/images/offer.jpg"
+					src="https://images.unsplash.com/photo-1632935187086-49a9d8027292?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1322"
 					alt="Offer"
 					width={1000}
 					height={1000}
 				/>
-			</div> */}
+			</div>
 		</div>
 	);
 }
